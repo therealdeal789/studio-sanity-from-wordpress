61dee207a1309f3b74b0979547b071e5c281ed08
Gradin <alexander@gradin.me>
2025-09-01 09:32:11 +0200

    first commit

---
+++ sanity.cli.ts
@@ -0,0 +1,19 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'xkpa15s8',
+    dataset: 'production'
+  },
+  /**
+   * Enable auto-updates for studios.
+   * Learn more at https://www.sanity.io/docs/cli#auto-updates
+   */
+  autoUpdates: true,
+  autoUpdates: true,
+  vite: {
+    server: {
+      host: '0.0.0.0',
+    },
+  },
+})

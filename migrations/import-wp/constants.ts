d4cde65c02cf7854e604b6955a29d358762c2922
Gradin <alexander@gradin.me>
2025-09-01 09:55:13 +0200

    Processing post types

--- migrations/import-wp/constants.ts
+++ migrations/import-wp/constants.ts
@@ -1,2 +1,12 @@
+import type {SanitySchemaType, WordPressDataType} from './types'
+
 export const BASE_URL = `https://aktier.se/wp-json/wp/v2`
 export const PER_PAGE = 100
+
+export const WP_TYPE_TO_SANITY_SCHEMA_TYPE: Record<WordPressDataType, SanitySchemaType> = {
+  categories: 'category',
+  posts: 'post',
+  pages: 'page',
+  tags: 'tag',
+  users: 'author',
+}
--- migrations/import-wp/index.ts
+++ migrations/import-wp/index.ts
@@ -1,29 +1,42 @@
+import {decode} from 'html-entities'
 import type {SanityDocumentLike} from 'sanity'
 import {createOrReplace, defineMigration} from 'sanity/migrate'
+import type {WP_REST_API_Post, WP_REST_API_Term, WP_REST_API_User} from 'wp-types'
 
+import {getDataTypes} from './lib/getDataTypes'
 import {wpDataTypeFetch} from './lib/wpDataTypeFetch'
 
-// This will import `post` documents into Sanity from the WordPress API
+// Allow the migration script to import a specific post type when run
 export default defineMigration({
-  title: 'Import WP',
+  title: 'Import WP JSON data',
 
   async *migrate() {
-    const wpType = 'posts'
+    const {wpType, sanityType} = getDataTypes(process.argv)
     let page = 1
     let hasMore = true
 
     while (hasMore) {
       try {
-        const wpData = await wpDataTypeFetch(wpType, page)
+        let wpData = await wpDataTypeFetch(wpType, page)
 
         if (Array.isArray(wpData) && wpData.length) {
           const docs: SanityDocumentLike[] = []
 
-          for (const wpDoc of wpData) {
+          for (let wpDoc of wpData) {
             const doc: SanityDocumentLike = {
-              _id: `post-${wpDoc.id}`,
-              _type: 'post',
-              title: wpDoc.title?.rendered.trim(),
+              _id: `${sanityType}-${wpDoc.id}`,
+              _type: sanityType,
+            }
+
+            if (wpType === 'posts' || wpType === 'pages') {
+              wpDoc = wpDoc as WP_REST_API_Post
+              doc.title = decode(wpDoc.title.rendered).trim()
+            } else if (wpType === 'categories' || wpType === 'tags') {
+              wpDoc = wpDoc as WP_REST_API_Term
+              doc.name = decode(wpDoc.name).trim()
+            } else if (wpType === 'users') {
+              wpDoc = wpDoc as WP_REST_API_User
+              doc.name = decode(wpDoc.name).trim()
             }
 
             docs.push(doc)
